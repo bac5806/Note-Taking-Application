72e588a64af59a19eba2652e735b049eee56a91c
Broc C <bac5806@gmail.com>
2021-04-25 23:10:49 -0400

    commiting code and heroku setup, needed to make new repository for for heroku

---
+++ README.md
@@ -0,0 +1,5 @@
+# Note-Taking-Application
+
+![alt text](Assets/note-taking.png)
+
+## This application allows the user to add, retrieve, and delete notes. The application uses the express npm package. The data is stored as json and is parsed and strigified into readable data.
---
+++ repos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void change(ref int x)
+        {
+            x += 5;
+        }
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x);
+
+            change(ref x);
+
+            while (x > 0)
+            {
+                Console.WriteLine(x);
+                --x;
+            }
+        }
+    }
+}
